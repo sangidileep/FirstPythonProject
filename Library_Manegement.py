3a0e16fba5dd517a589bb469eaae2167b270e79e
Shubham S Sirsat <shubahm.sirsat321@gmail.com>
2021-09-30 18:20:11 +0530

    Version 1.01

---
+++ Library_Manegement.py
@@ -0,0 +1,77 @@
+class Library:
+    def __init__(self, list, name):
+        self.bookslist = list
+        self.name = name
+        self.lendDict =  {}
+        
+    def displayBook(self):
+        print("We have followings books in our library: {self.name} ")
+        for book in self.booksList:
+            print(book)
+            
+    def lendBook(self,user, book):
+        if book not in self.lendDict.keys():
+            self.lendDict.update({book.user})
+            print("Lender-book database has been updated. You can take the book now")
+        else:
+            print(f"Book is already being used by {self.lendDict[book]}")
+            
+            
+    def addBook(self, book):
+        self.booksList.append(book)
+        print("book has been added to the book list")
+        
+    def returnBook(self,book):
+        self.booksList.remove(book)
+
+if __name__ == '__main__':
+    harry = Library(['Python', 'Rich daad poor dasssd', 'Harry potter', 'C++ Basics', 'Algorithms by CLRS'], "Code with shubham")
+    
+    while(True):
+        print("Welcome to the {harry.name} library. Enter your choice to continue")
+        print('1. Display Books')
+        print('2. Lend a Book')
+        print('3. Add a Book')
+        print('4. Return a Book')
+        
+        user_choice =int(input())
+        
+        if user_choice ==1:
+            harry.displayBook()
+            
+        elif user_choice ==2:
+            book = input("Enter the name of the book you want to lend:")
+            user = input("Enter your name")
+            harry.lendBook(user,book)
+            
+        elif user_choice == 3:
+            book = input("Ener the name of the book you enat to add:")
+            harry.addBook(book)
+            
+            
+        elif user_choice == 4:
+            book = input("Ener the name of the book you want to return:")
+            harry.returnBook(book)
+            
+            
+        else:
+            print("Not a valid option")
+            
+        print("Press q to quit and c to continue")
+        user_choice2 =""
+        while (user_choice2!="c" and user_choice2!="q"):
+            user_choice2 = input()
+        if user_choice2 == "q":
+            exit()
+            
+        if user_choice2 == "c":
+            continue
+            
+            
+        
+            
+            
+        
+    
+
+    
