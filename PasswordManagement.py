049de9d7d3db859eb6ffa74b5cd9f74ff38a9e72
Shubham S Sirsat <shubahm.sirsat321@gmail.com>
2021-10-05 19:22:35 +0530

    version 1.02

--- PasswordManagement.py
+++ PasswordManagement.py
@@ -11,16 +11,16 @@ while menu != '1' or menu !='2':
         username = input("Enter  your username for this Software or Website: ")
         password = input("Enter your password for this Software or Webside:  ")
         file = open("SecutedData.txt", 'a')
-        file.write('\nSoftwareName:  '+SoftwareName +"  Id:"+ username +'  Password:'+ password)
+        file.write('\nSoftwareName: '+SoftwareName +"  Id: "+ username +'  Password: '+ password)
         file.close()
 
     if menu =='2':
         file = open("SecutedData.txt", 'r')
-        print('soft or web \t username \t password','\n'
-              ,file.read())
-        # for i in file:
-        #     data = i.split(":")
-        #     print(data[0]+'\t'+data[2]+'\t'+data[3])
+        # print('soft or web \t username \t password','\n',file.read())
+        for i in file:
+            data = i.split(" ")
+            print(data[3:])
+            # print(data[0] + '\t' + data[2] + '\t' + data[3])
     if menu =='3':
         exit()
 
