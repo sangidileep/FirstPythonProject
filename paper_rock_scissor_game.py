0b61e714dc4915f313cf4e0d902d8297b0832b37
Shubham S Sirsat <shubahm.sirsat321@gmail.com>
2021-10-05 22:38:17 +0530

    version 1.00

---
+++ paper_rock_scissor_game.py
@@ -0,0 +1,39 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+options = ["paper", "rock", "scissor"]
+
+while True:
+    user_input = input("Eneter the option paper, scissor, rock or press 'Q' for exit : ").lower()
+    if user_input == 'q':
+        exit()
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    computer_pick = options[random_number]
+
+    print("computer_pick:", computer_pick + '.')
+
+    if user_input == 'paper' and computer_pick == 'rock':
+        print("You win!")
+        user_wins += 1
+    elif user_input == 'rock' and computer_pick == 'scissor':
+        print("You win!")
+        user_wins += 1
+    elif user_input == 'scissor' and computer_pick == 'paper':
+        print("You win!")
+        user_wins += 1
+    else:
+        print("compter win!")
+        computer_wins += 1
+
+print("Your Wins:", user_wins, 'times')
+print("computer Wins:", computer_wins, 'times')
+print("Goodbuy!")
+
+
+
+
+
