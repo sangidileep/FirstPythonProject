69628b8e29da0d4387b8f8748b998d2e8cdb898a
Shubham S Sirsat <shubahm.sirsat321@gmail.com>
2021-10-05 18:44:44 +0530

    New code is added for password manager Version 1.01

---
+++ PasswordManagement.py
@@ -0,0 +1,26 @@
+menu = ''
+while menu != '1' or menu !='2':
+    menu = input("Would you like to save a new password or view your old ones?"
+                 "\n1. Input new password"
+                 "\n2. view passwords"
+                 "\n3. Exit"
+                 "\n  Enter Number here:")
+
+    if menu =='1':
+        SoftwareName = input("Enter the name of the Software or Website: ")
+        username = input("Enter  your username for this Software or Website: ")
+        password = input("Enter your password for this Software or Webside:  ")
+        file = open("SecutedData.txt", 'a')
+        file.write('\nSoftwareName:  '+SoftwareName +"  Id:"+ username +'  Password:'+ password)
+        file.close()
+
+    if menu =='2':
+        file = open("SecutedData.txt", 'r')
+        print('soft or web \t username \t password','\n'
+              ,file.read())
+        # for i in file:
+        #     data = i.split(":")
+        #     print(data[0]+'\t'+data[2]+'\t'+data[3])
+    if menu =='3':
+        exit()
+
